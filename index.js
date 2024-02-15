0684f6b62d5598fe4203d08a3f30bd0b4dc48075
Gaurav Koli <101950880+Gk091202@users.noreply.github.com>
2024-02-15 11:29:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let prevScrollPos = window.pageYOffset;
 
-window.onscroll = function () {
-    let currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-        document.querySelector('.navbar').classList.remove('hidden');
-    } else {
-        document.querySelector('.navbar').classList.add('hidden');
-    }
-
-    prevScrollPos = currentScrollPos;
-};
